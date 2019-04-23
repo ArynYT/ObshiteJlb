7294f85df525f3298732f700c25a46132711d859
ArynYT <49909132+ArynYT@users.noreply.github.com>
2019-04-23 19:17:38 +0600

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,24 @@
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+print( Fore.BLACK )
+print( Back.YELLOW )
+
+priv = input("Привет, меня зовут Бот-общитель. А тебя? ")
+
+print( Back.GREEN )
+
+print("Думаю пожружимся " + priv + ")))")
+
+dela = input("Как дела? ")
+
+poch = input("Почему? ")
+
+print( Back.WHITE )
+
+print("Понятно. ")
+print("Нажми [ENTER] что-бы выйти!")
+print("Исходный код: https://github.com/ArynYT/ObshiteJlb/blob/master/test4.py ")
+input()
